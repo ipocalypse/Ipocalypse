b90eaac36152aeb5f72941d43e53ca6ccb2ead6a
Grif Priest <Grif@Grifs-Mac-mini.local>
2011-11-05 16:11:23 -0700

    Initial Commit

---
+++ Ipocalypse/IpocalypseAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  IpocalypseAppDelegate.h
+//  Ipocalypse
+//
+//  Created by Grif on 11/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class IpocalypseViewController;
+
+@interface IpocalypseAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) IpocalypseViewController *viewController;
+
+@end
---
+++ Ipocalypse/IpocalypseViewController.h
@@ -0,0 +1,13 @@
+//
+//  IpocalypseViewController.h
+//  Ipocalypse
+//
+//  Created by Grif on 11/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IpocalypseViewController : UIViewController
+
+@end
